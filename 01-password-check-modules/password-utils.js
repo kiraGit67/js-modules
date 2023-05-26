3f8184327954eb3a610f95e38aabb69470777f20
kiraGit67 <kirsten@sais.de>
2023-05-26 18:39:12 +0200

    feat: adds export function isValid into password-utils.js

--- 01-password-check-modules/password-utils.js
+++ 01-password-check-modules/password-utils.js
@@ -1,5 +1,20 @@
 "use strict";
 
+import {
+  containsNumbers,
+  containsLowerCase,
+  containsUpperCase,
+} from "./string-utils.js";
+
 export function atLeast10charsLong(str) {
   return str.length >= 10;
+}
+
+export function isValid(pw) {
+  return (
+    containsNumbers(pw) &&
+    containsLowerCase(pw) &&
+    containsUpperCase(pw) &&
+    atLeast10charsLong(pw)
+  );
 }
