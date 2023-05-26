bcca36f168435d516b75212ade6a8d99662aab7a
kiraGit67 <kirsten@sais.de>
2023-05-26 19:24:37 +0200

    feat: extends togglePasswords function by conditional btn-styling

--- 01-password-check-modules/utils/input-utils.js
+++ 01-password-check-modules/utils/input-utils.js
@@ -5,9 +5,13 @@ export function togglePasswords(input1, input2, btn) {
     input1.setAttribute("type", "text");
     input2.setAttribute("type", "text");
     btn.innerText = "Hide Passwords";
+    btn.classList.add("btn--hide");
+    btn.classList.remove("btn--show");
   } else {
     input1.setAttribute("type", "password");
     input2.setAttribute("type", "password");
     btn.innerText = "Show Passwords";
+    btn.classList.add("btn--show");
+    btn.classList.remove("btn--hide");
   }
 }
